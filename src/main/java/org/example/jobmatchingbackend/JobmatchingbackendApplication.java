9727bfaaba74b04e9a5ac2fb9644c1412f10b188
saikrishnamudiraj <saikrishna123.mudiraj@gmail.com>
2025-08-14 18:57:58 +0530

    Initial commit

---
+++ src/main/java/org/example/jobmatchingbackend/JobmatchingbackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobmatchingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmatchingbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobmatchingbackendApplication.class, args);
+    }
+
+}
